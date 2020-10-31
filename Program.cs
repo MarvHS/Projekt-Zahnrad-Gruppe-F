3ff503ded1c1096ae64876c16bbd9871927e0856
KevinGrueter <71759955+KevinGrueter@users.noreply.github.com>
2020-10-31 21:22:42 +0100

    Änderung
    Moin ihr beiden,
    Ich habe Material, Volumen, Preis, Masse als Variablen hinzugefügt.
    Außerdem habe ich aus dem double von z ein int gemacht. -> Das gibt der Nutzer ein... keine Kommerzahl.

--- Program.cs
+++ Program.cs
@@ -3,35 +3,46 @@ using System;
 
 namespace ProjektZahnrad
 {
+    enum Material { Holz, Metal, Kunstoff, NichtEisenMetalle } //Deklaration des Materials (Eingang)
+    
     class Program
     {
-         static void main(string[] args)
+        public static void Main(string[] args)
         {
-           double m, d, b, z;                   // Deklaration der Variablen (Eingang)
-           double p, da, df, h, ha, hf, c, a;   // Deklaration der Variablen (Ausgang)            
-            
+            double m, d, b;                  // Deklaration der Variablen (Eingang)
+            int z;                          //Deklaration der Variablen (Eingang)
 
-            Console.WriteLine("Geben Sie die Eingangsparameter für die Zahnradberechnung ein.");
+            Material M;                     //Deklaration des Materials (Eingang)
+
+       
+
+
+			double p, da, df, h, ha, hf, c, a;   // Deklaration der Variablen (Ausgang)            
+
+            double v, G, P;   //Deklaration der Basisinformationen Volumen, Masse, Preis (Ausgang)
+
+
+			Console.WriteLine("Geben Sie die Eingangsparameter für die Zahnradberechnung ein.");
             Console.WriteLine();
 
 
             //Initialisierung der Variablen
            
             d = Prüfung("Geben Sie den Teilkreisdurchmesser des Zahnrads in mm an: ");
-           
-            b = Prüfung("Geben Sie die Breite des Zahnrads in mm an: ");
-      
-            z = Prüfung("Geben Sie die Zähnezahl des Zahnrads an: ");
-          
+
+			b = Prüfung("Geben Sie die Breite des Zahnrads in mm an: ");
+
+			z = (int)Prüfung("Geben Sie die Zähnezahl des Zahnrads an: ");
 
 
-            //Berechnungen der Ausgangswerte
 
-            m = d / z;
+        //Berechnungen der Ausgangswerte
+
+			m = d / z;
             Console.WriteLine("Modul: " + m);
 
-            p = Math.PI * m;
-            Console.WriteLine("Teilung: "+p);
+			p = Math.PI* m;
+			Console.WriteLine("Teilung: "+p);
 
             da = d + 2 * m;
             Console.WriteLine("Kopfkreisdurchmesser: " + da);
@@ -50,27 +61,39 @@ namespace ProjektZahnrad
 
             hf = m + c;
             Console.WriteLine("Zahnfußhöhe: " + hf);
+            v = 0;
+            Console.WriteLine("Volumen: "+v);
+            G= 0;
+            Console.WriteLine("Masse: " + G);
+
+            P = 0;
+            Console.WriteLine("Preis: " + P);
+
+
+
+
 
         }
 
         // Funktion zur Überprüfung der Eingangswerte ( Werte größer Null)
-        static double Prüfung (string name)
+        static double Prüfung(string name)
+    {
+        double xx = 0;
+        do
         {
-            double xx = 0;
-            do
-            {
-                Console.WriteLine(name);
-                xx = Convert.ToDouble(Console.ReadLine());
-                if (xx <= 0)
-                    Console.WriteLine("Geben Sie eine Zahl größer Null ein!");
-            } while (xx <= 0);
+            Console.WriteLine(name);
+            xx = Convert.ToDouble(Console.ReadLine());
+            if (xx <= 0)
+                Console.WriteLine("Geben Sie eine Zahl größer Null ein!");
+        } while (xx <= 0);
 
 
-            return (xx);
-           
-        }
-        
-        
-        
+        return (xx);
+
     }
+
+
+
 }
+}
+
